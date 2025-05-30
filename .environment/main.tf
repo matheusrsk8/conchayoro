896f5cf74c22dbb6bdd8ec6ec5b12f470651fdb3
Matheus Duarte Rocha <75253548+matheusrsk8@users.noreply.github.com>
2025-05-30 02:57:23 +0000

    update main tf

--- .environment/main.tf
+++ .environment/main.tf
@@ -6,17 +6,26 @@ resource "aws_ecr_repository" "cyo_ecr_repo" {
   image_scanning_configuration {
     scan_on_push = true
   }
+
+  # Impede destruição acidental
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_elastic_beanstalk_application" "cyo_eba" {
-  name        = "${var.PROJECT_NAME}"
+  name        = var.PROJECT_NAME
   description = "Project application"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_elastic_beanstalk_environment" "cyo_ebef" {
-  name                = "${var.MODULE_NAME}"
+  name                = var.MODULE_NAME
   application         = aws_elastic_beanstalk_application.cyo_eba.name
-  solution_stack_name = "${var.SOLUTION_STACK_NAME}"
+  solution_stack_name = var.SOLUTION_STACK_NAME
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
@@ -27,13 +36,13 @@ resource "aws_elastic_beanstalk_environment" "cyo_ebef" {
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
-    value     = "${var.EnvironmentType}"
+    value     = var.EnvironmentType
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "LoadBalancerType"
-    value     = "${var.LoadBalancerType}"
+    value     = var.LoadBalancerType
   }
 
   setting {
@@ -57,36 +66,40 @@ resource "aws_elastic_beanstalk_environment" "cyo_ebef" {
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MinSize"
-    value     = "${var.MinSize}"
+    value     = var.MinSize
   }
 
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
-    value     = "${var.MaxSize}"
+    value     = var.MaxSize
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:command"
     name      = "DeploymentPolicy"
-    value     = "${var.DeploymentPolicy}"
+    value     = var.DeploymentPolicy
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:command"
     name      = "BatchSizeType"
-    value     = "${var.BatchSizeType}"
+    value     = var.BatchSizeType
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:command"
     name      = "BatchSize"
-    value     = "${var.BatchSize}"
+    value     = var.BatchSize
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:command"
     name      = "Timeout"
-    value     = "${var.Timeout}"
+    value     = var.Timeout
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
