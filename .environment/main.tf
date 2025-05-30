0da37fcc214d6e5ef4f7005afc84ac78c86f4dd1
Matheus Duarte Rocha <75253548+matheusrsk8@users.noreply.github.com>
2025-05-30 04:04:10 +0000

    d

--- .environment/main.tf
+++ .environment/main.tf
@@ -15,7 +15,7 @@ resource "aws_elastic_beanstalk_application" "cyo_eba" {
 resource "aws_elastic_beanstalk_environment" "cyo_ebef" {
   name = "${var.MODULE_NAME}"
   application = aws_elastic_beanstalk_application.cyo_eba.name
-  solution_stack_name = "${var.SOLUTION_STACK_NAME}"
+  solution_stack_name = "64bit Amazon Linux 2023 v4.5.2 running Docker"
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
